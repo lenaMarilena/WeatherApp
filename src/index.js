4feffdeac3a66d5fbf8008f79da5a79c1af5da4e
lenaMarilena <112854347+lenaMarilena@users.noreply.github.com>
2022-09-30 17:14:44 +0200

    changes of design

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
-function whatCurrentDate() {
-  let nowDate = new Date();
+//loader
+const mask = document.querySelector(".mask");
 
-  let week = [
+window.addEventListener("load", () => {
+  mask.classList.add("hide");
+  setTimeout(() => {
+    mask.remove();
+  }, 600);
+});
+
+//getting elements
+const dateHeading = document.querySelector("#current-date");
+
+const searchForm = document.querySelector("#search-forecast");
+const searchedCity = document.querySelector("#searched-city");
+
+const weatherTemp = document.querySelector("#current-display-temp");
+const weatherDesc = document.querySelector("#current-display-desc");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const feelsLike = document.querySelector("#feels-like");
+const visibility = document.querySelector("#visibility");
+const minTemp = document.querySelector("#temp-low");
+const maxTemp = document.querySelector("#temp-high");
+
+let mainIcon = document.querySelector("#main-icon");
+
+const celsius = document.querySelector("#celsius");
+const fahrenheit = document.querySelector("#fahrenheit");
+
+const locationBtn = document.querySelector(".form-icon");
+
+//API key
+const apiKey = "62231151ce343c4d68652e1617efc22f";
+
+//creating global variables that will store the celsius Temp
+let celsiusTemp = null;
+let maxCelsTemp = null;
+let minCelsTemp = null;
+let feelsLikeTemp = null;
+
+//setting current date
+const setDate = () => {
+  let date = new Date();
+  const days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -10,98 +51,212 @@ function whatCurrentDate() {
     "Friday",
     "Saturday",
   ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  dateHeading.innerHTML = `${days[date.getDay()]}, ${date.getDate()} ${
+    months[date.getMonth()]
+  }`;
+};
 
-  let dayIndex = nowDate.getDay();
-  let dayOfWeek = week[dayIndex];
-  let hours = nowDate.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
+setDate();
+
+//function for converting date value from the api to normal state
+const formatDate = (timestamp) => {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay(); //we are getting value from 0 to 6
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+};
+
+//function for displaying weather for week
+const displayWeekWeather = (response) => {
+  let dailyForecast = response.data.daily;
+  const forecast = document.querySelector("#week-forecast");
+  let forecastHTML = "";
+  for (let i = 1; i < 7; i++) {
+    forecastHTML += `
+        <div class="col-2">
+             <div class="weather-card">
+                 <div class="card-date" id="card-date">${formatDate(
+                   dailyForecast[i].dt
+                 )}</div>
+                 <div class="card-image">
+                    <img src="img/icons/${
+                      dailyForecast[i].weather[0].icon
+                    }.png" alt="Sun, clouds and rain" width="50px" height="50px" />
+                 </div>
+                 <div class="card-temp">
+                     <span id="card-temp-high">${Math.round(
+                       dailyForecast[i].temp.max
+                     )}</span>°/ <span id="card-temp-low">${Math.round(
+      dailyForecast[i].temp.min
+    )}</span>°
+                 </div>
+             </div>
+         </div>
+        `;
   }
 
-  let minutes = nowDate.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+  forecast.innerHTML = forecastHTML;
+};
 
-  let dateToday = document.querySelector("#last-update");
-  dateToday.innerHTML = `Last update: ${dayOfWeek}, ${hours}:${minutes}`;
-}
+const showWeekWeather = (coords) => {
+  console.log(coords);
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`
+    )
+    .then(displayWeekWeather);
+};
 
-function toFahrenheit() {
-  let celsius = 28;
-  let fahrenheit = Math.round(celsius * (9 / 5) + 32);
-  let valueMax = document.querySelector(".value-max");
-  valueMax.innerHTML = `${fahrenheit}`;
-}
-function toCelsius() {
-  let valueMax = document.querySelector(".value-max");
-  valueMax.innerHTML = "28";
-}
+//weather API integration
+const displayWeather = (response) => {
+  celsiusTemp = Math.round(response.data.main.temp);
+  searchedCity.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  weatherTemp.innerHTML = `${celsiusTemp}°`;
+  weatherDesc.innerHTML = `${response.data.weather[0].main}`;
 
-whatCurrentDate();
+  maxCelsTemp = Math.round(response.data.main.temp_max);
+  minCelsTemp = Math.round(response.data.main.temp_min);
+  maxTemp.innerHTML = `${maxCelsTemp}`;
+  minTemp.innerHTML = `${minCelsTemp}`;
 
-let fahrenheitValue = document.querySelector(".fahrenheit");
-fahrenheitValue.addEventListener("click", toFahrenheit);
+  //changing icon
+  let apiIcon = response.data.weather[0].icon;
+  mainIcon.setAttribute("src", `img/icons/${apiIcon}.png`);
 
-let celsiusValue = document.querySelector(".celsius");
-celsiusValue.addEventListener("click", toCelsius);
+  //more info block
+  feelsLikeTemp = Math.round(response.data.main.feels_like);
+  feelsLike.innerHTML = `${feelsLikeTemp}`;
+  humidity.innerHTML = `${response.data.main.humidity}`;
+  wind.innerHTML = `${Math.round(response.data.wind.speed)}`;
+  visibility.innerHTML = `${response.data.visibility / 1000}`;
 
-function showCurrentCityTemp(response) {
-  let currentMaxTemp = Math.round(response.data.main.temp_max);
-  let currentMinTemp = Math.round(response.data.main.temp_min);
-  let currentCity = response.data.name;
-  let maxTemp = document.querySelector("#value-max");
-  let minTemp = document.querySelector("#value-min");
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = currentCity;
-  maxTemp.innerHTML = currentMaxTemp;
-  minTemp.innerHTML = currentMinTemp;
-  iconeElement.setAttribute("alt", response.data.weather[0].description);
+  showWeekWeather(response.data.coord);
+};
 
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#humidity").innerHTML = Math.round(
-    response.data.main.humidity
-  );
-  document.querySelector("#now-status").innerHTML =
-    response.data.weather[0].description;
-}
+const searchData = (city) => {
+  //getting API key
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    )
+    .then(displayWeather);
+};
 
-function showLocationByPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "62231151ce343c4d68652e1617efc22f";
-  let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather";
-  let units = "metric";
-  let apiUrl = `${apiEndPoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showCurrentCityTemp);
-}
+searchData("Chicago");
 
-function getCurrentLocation() {
-  navigator.geolocation.getCurrentPosition(showLocationByPosition);
-}
+//geolocation processing
+const showLocation = () => {
+  let latitude;
+  let longitude;
+  navigator.geolocation.getCurrentPosition((position) => {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+      )
+      .then(displayWeather);
+  });
+};
 
-let currentButton = document.querySelector("#current-location");
-currentButton.addEventListener("click", getCurrentLocation);
+locationBtn.addEventListener("click", showLocation);
 
-function showLocationByCity(cityName) {
-  let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let units = "metric";
-  let apiUrlCity = `${apiEndpoint}?q=${cityName}&appid=${apiKey}&units=${units}`;
-  console.log(apiUrlCity);
-  axios.get(apiUrlCity).then(showCurrentCityTemp);
-}
+//unit conversion
+const toFahrenheit = (value) => {
+  let res = Math.round((value * 9) / 5 + 32);
+  return res;
+};
 
-function showCheckedCityTemp(event) {
-  event.preventDefault();
+const toCelsius = (value) => {
+  let res = Math.round(((value - 32) * 5) / 9);
+  return res;
+};
 
-  let input = document.querySelector("#your-city");
-  showLocationByCity(input.value);
-}
+const convertToFahr = () => {
+  let fahrenheitValue = toFahrenheit(celsiusTemp);
+  weatherTemp.innerHTML = `${fahrenheitValue}°`;
+  maxTemp.innerHTML = `${toFahrenheit(maxCelsTemp)}`;
+  minTemp.innerHTML = `${toFahrenheit(minCelsTemp)}`;
+  feelsLike.innerHTML = `${toFahrenheit(feelsLikeTemp)}`;
 
-showLocationByCity("Madrid");
+  //calculations for weekly forecast values
+  let weeklyMin = document.querySelectorAll("#card-temp-low");
+  weeklyMin.forEach((item) => {
+    //grabbing a value to convert
+    let minValue = item.innerHTML;
+    item.innerHTML = `${toFahrenheit(minValue)}`;
+  });
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", showCheckedCityTemp);
+  let weeklyMax = document.querySelectorAll("#card-temp-high");
+  weeklyMax.forEach((item) => {
+    //grabbing a value to convert
+    let minValue = item.innerHTML;
+    item.innerHTML = `${toFahrenheit(minValue)}`;
+  });
+
+  //removing and adding classes for buttons
+  fahrenheit.classList.add("active");
+  celsius.classList.remove("active");
+
+  //removing eventListener for fahrenehit to prevent multiple calculations and adding for celsius
+  celsius.addEventListener("click", convertToCelsius);
+  fahrenheit.removeEventListener("click", convertToFahr);
+};
+
+const convertToCelsius = () => {
+  weatherTemp.innerHTML = `${celsiusTemp}°`;
+  maxTemp.innerHTML = `${maxCelsTemp}`;
+  minTemp.innerHTML = `${minCelsTemp}`;
+  feelsLike.innerHTML = `${feelsLikeTemp}`;
+
+  //calculations for weekly forecast values
+  let weeklyMin = document.querySelectorAll("#card-temp-low");
+  weeklyMin.forEach((item) => {
+    //grabbing a value to convert
+    let minValue = item.innerHTML;
+    item.innerHTML = `${toCelsius(minValue)}`;
+  });
+
+  let weeklyMax = document.querySelectorAll("#card-temp-high");
+  weeklyMax.forEach((item) => {
+    //grabbing a value to convert
+    let minValue = item.innerHTML;
+    item.innerHTML = `${toCelsius(minValue)}`;
+  });
+
+  //removing and adding classes for buttons
+  fahrenheit.classList.remove("active");
+  celsius.classList.add("active");
+
+  //removing eventListener for celsius to prevent multiple calculations and adding for fahrenheit
+  fahrenheit.addEventListener("click", convertToFahr);
+  celsius.removeEventListener("click", convertToCelsius);
+};
+
+//adding listeners for units' buttons
+fahrenheit.addEventListener("click", convertToFahr);
+celsius.addEventListener("click", convertToCelsius);
+
+//working with search form
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchInput = document.querySelector(".form-input");
+  searchData(searchInput.value);
+  fahrenheit.classList.remove("active");
+  celsius.classList.add("active");
+  fahrenheit.addEventListener("click", convertToFahr);
+});
